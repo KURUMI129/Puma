0164603055382fee905a487b212b5164b8941262
Karol Levi Tafolla Salazar <karollevitafollasalazar@gmail.com>
2025-10-24 15:34:46 -0600

    24 de Octubre v2

--- src/app/services/productos.ts
+++ src/app/services/productos.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Iisproductos } from '../..models/is.Model';
+import { Iisproductos } from '../models/is.Model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,30 +7,35 @@ import { Iisproductos } from '../..models/is.Model';
 export class Productos {
   private productos: Iisproductos[] = [
     {
+      photo: '/public/Recursos/Articulos/descarga.jpeg',
       name: 'Producto 1',
       description: 'Descripcion del producto 1',
       price: 100,
       sport: ['Running', 'Futbol']
     }, 
     {
+      photo: '',
       name: '',
       description: '',
       price: 100,
       sport: ['Running', 'Futbol']
     }, 
     {
+      photo: '',
       name: '',
       description: '',
       price: 100,
       sport: ['Running', 'Futbol']
     }, 
     {
+      photo: '',
       name: '',
       description: '',
       price: 100,
       sport: ['Running', 'Futbol']
     }, 
     {
+      photo: '',
       name: '',
       description: '',
       price: 100,
@@ -38,7 +43,7 @@ export class Productos {
     }
   ];
 
-  obtenerProductos(): producto[] {
+  obtenerProductos(): Iisproductos[] {
     return this.productos;
   }
 }
