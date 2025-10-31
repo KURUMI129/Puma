fe3343f3f779148ba03072d01602485122894c55
Karol Levi Tafolla Salazar <karollevitafollasalazar@gmail.com>
2025-10-31 12:20:04 -0600

    31 de Octubre

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { Home } from './components/home/home';
-import { Header } from './components/header/header';
-import { Bartest } from './components/bartest/bartest';
-import { Login } from './components/login/login';
+ import { RouterOutlet } from '@angular/router';
+ import { CommonModule } from '@angular/common';
+ import { Home } from './components/home/home';
+ import { Header } from './components/header/header';
+ import { Bartest } from './components/bartest/bartest';
+ import { Login } from './components/login/login';
+ import { Footer } from './components/footer/footer'; 
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, Home, Header, Bartest, Login],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('Store-Online');
+ @Component({
+   selector: 'app-root',
+   imports: [RouterOutlet, CommonModule, Home, Header, Bartest, Login, Footer], 
+   templateUrl: './app.html',
+   styleUrl: './app.css'
+ })
+ export class App {
+   protected readonly title = signal('Store-Online');
+   
+   showHeader = true;
 
-  showHeader = true;
-
-  /**
-     * Esta es una "función" o "método".
-     * Se activa cuando una nueva página se carga en el <router-outlet>.
-     */
-  onComponentLoad(component: any) {
-    // Revisa si el componente que se cargó es la página de Login.
-    // Si es el Login, pone 'showHeader' en 'false' para ocultar el header.
-    this.showHeader = !(component instanceof Login);
-  }
-}
+ /**
+    * Esta es una "función" o "método".
+    * Se activa cuando una nueva página se carga en el <router-outlet>.
+    */
+   onComponentLoad(component: any) {
+     // Revisa si el componente que se cargó es la página de Login.
+     // Si es el Login, pone 'showHeader' en 'false' para ocultar el header.
+     this.showHeader = !(component instanceof Login);
+   }
+ }
--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+ import { CommonModule } from '@angular/common'; // Importado
+ import { RouterModule } from '@angular/router'; // Importado
 
-@Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css'
-})
-export class Footer {
+ @Component({
+   selector: 'app-footer',
+   imports: [CommonModule, RouterModule], // Añadido
+   templateUrl: './footer.html',
+   styleUrl: './footer.css'
+ })
+ export class Footer {
 
-}
+ }
