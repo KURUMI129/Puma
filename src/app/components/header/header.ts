c15f4bd48615b258d4e7474dbbf59e523d4f3fb6
KURUMI129 <144570610+KURUMI129@users.noreply.github.com>
2025-12-01 20:37:51 +0000

    Co-authored-by: JesusAV160122 <JesusAV160122@users.noreply.github.com>

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgForOf } from '@angular/common';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   imports: [NgForOf, CommonModule],
@@ -7,16 +9,21 @@ import { CommonModule, NgForOf } from '@angular/common';
   styleUrl: './header.css'
 })
 export class Header {
-  // public headerOptions: string[] = ['Lo Nuevo', 'Hombre', 'Mujer', 'Niño/a', 'Ofertas', 'SNKRS'];
+  
   public headerOptions: { name: string, subOptions: string[] }[] = [
     { name: 'Lo Nuevo', subOptions: ['Novedades', 'Lanzamientos', 'Tendencias'] },
     { name: 'Hombre', subOptions: ['Calzado', 'Ropa', 'Accesorios'] },
     { name: 'Mujer', subOptions: ['Calzado', 'Ropa', 'Accesorios'] },
     { name: 'Niño/a', subOptions: ['Niños', 'Adolescentes'] },
     { name: 'Ofertas', subOptions: ['Descuentos', 'Promociones Especiales'] },
-    { name: 'SNKRS', subOptions: ['Lanzamientos Exclusivos', 'Colecciones'] }
-  ]
-  public headerIcons: string[] = ['<i class="bi bi-arrow-through-heart-fill"></i>', '<i class="bi bi-bag-check-fill"></i>'];
+    { name: 'SNKRS', subOptions: ['Lanzamientos Exclusivos', 'Colecciones'] }
+  ];
+
+  public headerIcons: string[] = [
+    '<i class="bi bi-arrow-through-heart-fill"></i>', 
+    '<i class="bi bi-bag-check-fill"></i>'
+  ];
+
   // Declaracion de los productos
   articleObj = {
     photo: '',
@@ -24,6 +31,14 @@ export class Header {
     description: '',
     clasification: '',
     price: ''
-  }
+  };
   articleList : any = [];
+
+  constructor(private router: Router) {}
+
+  // Función para cerrar sesión
+  logout() {
+    localStorage.removeItem('usuarioLogueado'); // Borra la sesión
+    this.router.navigate(['/login']); // Redirige al login
+  }
 }
